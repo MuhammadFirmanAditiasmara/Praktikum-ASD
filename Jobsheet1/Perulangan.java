6f0ec824e8eeaf6b92420d15ab152d13af9960d1
Adidtiasmara <mfirmanadid@gmail.com>
2025-02-13 09:21:39 +0700

    Praktikum Perulangan

--- Jobsheet1/Perulangan.java
+++ Jobsheet1/Perulangan.java
@@ -16,17 +16,18 @@ public class Perulangan {
 
         if (n < 10) {
             n += 10;
+            System.out.println("n : " + n);
         }
 
         for (int i = 1; i <= n; i++) {
             if (i == 6 || i == 10) {
                 continue;
             }
-            if ( i % 2 == 0) {
+            if (i % 2 == 0) {
                 System.out.print(i);
             }
 
-            if ( i % 2 != 0 ) {
+            if (i % 2 != 0) {
                 System.out.print("*");
             }
 
