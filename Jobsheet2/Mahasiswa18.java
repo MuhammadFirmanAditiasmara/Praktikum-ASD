15869f927b6b9ad5bd4f3f52d418f00334946dac
Adidtiasmara <mfirmanadid@gmail.com>
2025-02-20 08:00:33 +0700

    Adding File Mahasiswa18

---
+++ Jobsheet2/Mahasiswa18.java
@@ -0,0 +1,42 @@
+
+public class Mahasiswa18 {
+
+    
+        String nama;
+        String nim;
+        String kelas;
+        double ipk;
+
+    
+    void tampilkanInformasi() {
+        System.out.println("Nama : " + nama);
+        System.out.println("NIM : " + nim);
+        System.out.println("IPK : " + ipk);
+        System.out.println("Kelas : " + kelas);
+    }
+
+    void ubahKeklas (String kelasBaru){
+        kelas = kelasBaru;
+
+    }
+
+    void updateIpk(double ipkBaru){
+        if (ipk <= 0.0 || ipk >= 4.0) {
+            System.out.println("IPK tidak valid");  
+            
+        }
+        ipk = ipkBaru;
+    }
+
+    String nilaiKinerja() {
+        if (ipk >= 3.5) {
+            return "Kinerja sangat baik ";
+        } else if (ipk >= 3.0) {
+            return "Kinerja baik";
+        } else if (ipk >= 2.0) {
+            return "Kinerja cukup";
+        } else {
+            return "Kinerja kurang";
+        }
+    }
+}
