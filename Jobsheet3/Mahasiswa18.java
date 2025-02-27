d1af0331c459cc161c7700251328c101f8e282dd
Adidtiasmara <mfirmanadid@gmail.com>
2025-02-27 11:48:41 +0700

    Adding Perubahan Modifikasi Mahasiswa18

--- Jobsheet3/Mahasiswa18.java
+++ Jobsheet3/Mahasiswa18.java
@@ -4,6 +4,13 @@ public class Mahasiswa18{
     public String kelas;
     public float ipk;
 
-}
+    public void cetakInfo(){
 
+        System.out.println("NIM     : " + nim);
+        System.out.println("Nama    : " + nama);
+        System.out.println("Kelas   : " + kelas);
+        System.out.println("IPK     : " + ipk);
+        System.out.println("---------------------------------------");
+    }
+}
 
