dd7852f06f7ea1c411efc6e1e6cec814a21b0697
Adidtiasmara <mfirmanadid@gmail.com>
2025-02-28 23:38:15 +0700

    Adding File Dosen18

---
+++ Jobsheet3/Dosen18.java
@@ -0,0 +1,19 @@
+
+public class Dosen18 {
+
+    public String kode;
+    public String nama;
+    public boolean jenisKelamin;
+    public int usia;
+
+    public Dosen18() {
+
+    }
+
+    public Dosen18(String kode, String nama, boolean jenisKelamin, int usia) {
+        this.kode = kode;
+        this.nama = nama;
+        this.jenisKelamin = jenisKelamin;
+        this.usia = usia;
+    }
+}
