113b6361e26f8ad1ac4a2f7f071e9974e1d362e2
Adidtiasmara <mfirmanadid@gmail.com>
2025-03-20 12:20:19 +0700

    Adding File MahasiswaBerprestasi18 Praktikum 2

--- Jobsheet6/MahasiswaBerprestasi18.java
+++ Jobsheet6/MahasiswaBerprestasi18.java
@@ -31,4 +31,18 @@ public class MahasiswaBerprestasi18 {
             }
         }
     }
+
+    void SelectionSort() {
+        for (int i = 0; i < listMhs.length; i++) {
+            int idxMin = i;
+            for (int j = i + 1; j < listMhs.length; j++) {
+                if (listMhs[j].ipk < listMhs[idxMin].ipk) {
+                    idxMin = j;
+                }
+            }
+            Mahasiswa18 tmp = listMhs[idxMin];
+            listMhs[idxMin] = listMhs[i];
+            listMhs[i] = tmp;
+        }
+    }
 }
