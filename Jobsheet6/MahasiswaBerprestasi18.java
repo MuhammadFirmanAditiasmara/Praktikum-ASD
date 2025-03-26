f0d0e5339c54d5b6f78cd82281f07a13e57fac46
Adidtiasmara <mfirmanadid@gmail.com>
2025-03-26 23:17:05 +0700

    Adding File MahasiswaBerprestasi18

--- Jobsheet6/MahasiswaBerprestasi18.java
+++ Jobsheet6/MahasiswaBerprestasi18.java
@@ -1,6 +1,9 @@
 
+import java.util.Scanner;
+
 public class MahasiswaBerprestasi18 {
 
+    Scanner scan = new Scanner(System.in);
     Mahasiswa18[] listMhs = new Mahasiswa18[5];
     int idx;
 
@@ -10,6 +13,24 @@ public class MahasiswaBerprestasi18 {
             idx++;
         } else {
             System.out.println("data sudah penuh");
+        }
+    }
+
+    // Modifikasi Pertanyaan
+    void input() {
+        for (int i = 0; i < listMhs.length; i++) {
+            System.out.println("Masukkan data Mahasiswa ke-" + (i + 1));
+            listMhs[i] = new Mahasiswa18();
+            System.out.print("NIM\t: ");
+            listMhs[i].nim = scan.next();
+            System.out.print("Nama\t: ");
+            listMhs[i].nama = scan.next();
+            System.out.print("Kelas\t: ");
+            listMhs[i].kelas = scan.next();
+            System.out.print("IPK\t: ");
+            listMhs[i].ipk = scan.nextDouble();
+            
+            
         }
     }
 
@@ -32,6 +53,7 @@ public class MahasiswaBerprestasi18 {
         }
     }
 
+    // Percobaan 3
     void SelectionSort() {
         for (int i = 0; i < listMhs.length; i++) {
             int idxMin = i;
@@ -43,6 +65,21 @@ public class MahasiswaBerprestasi18 {
             Mahasiswa18 tmp = listMhs[idxMin];
             listMhs[idxMin] = listMhs[i];
             listMhs[i] = tmp;
+        }
+    }
+
+    // Percobaan 4
+    void InstertionSort() {
+        for (int i = 1; i < listMhs.length; i++) {
+            Mahasiswa18 temp = listMhs[i];
+            int j = i;
+            while (j > 0 && listMhs[j - 1].ipk > temp.ipk) {
+            // modifikasi pertanyaan DESC
+            // while (j > 0 && listMhs[j - 1].ipk < temp.ipk) {
+                listMhs[j] = listMhs[j - 1];
+                j--;
+            }
+            listMhs[j] = temp;
         }
     }
 }
