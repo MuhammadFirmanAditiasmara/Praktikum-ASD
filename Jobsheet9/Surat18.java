3d8437b827b49953c248c8456f74a95b03606f63
Adidtiasmara <mfirmanadid@gmail.com>
2025-05-13 09:28:27 +0700

    Add file Surat18 for Latihan Praktikum

---
+++ Jobsheet9/Surat18.java
@@ -0,0 +1,23 @@
+
+public class Surat18 {
+
+    String idSurat;
+    String namaMahasiswa;
+    String kelas;
+    char jenisIzin;
+    int durasi;
+
+    public  Surat18() {
+
+    }
+
+    public Surat18(String idSurat, String namaMahasiswa, String kelas, char jenisIzin, int durasi) {
+        this.idSurat = idSurat;
+        this.namaMahasiswa = namaMahasiswa;
+        this.kelas = kelas;
+        this.jenisIzin = jenisIzin;
+        this.durasi = durasi;
+
+    }
+
+}
